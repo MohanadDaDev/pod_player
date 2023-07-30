bb6c6ca5b59707e92079362dcedcd982540132ee
MohanadDaDev <mohanad.da.dev@gmail.com>
2023-07-30 05:53:16 +0300

    change look

--- lib/src/widgets/core/overlays/overlays.dart
+++ lib/src/widgets/core/overlays/overlays.dart
@@ -12,50 +12,54 @@ class _VideoOverlays extends StatelessWidget {
     final podCtrs = Get.find<PodGetXVideoController>(tag: tag);
 
     ///Built in overlay
-    return GetBuilder<PodGetXVideoController>(
-      tag: tag,
-      id: 'overlay',
-      builder: (podCtr) {
-        return AnimatedOpacity(
-          duration: const Duration(milliseconds: 200),
-          opacity: podCtr.isOverlayVisible ? 1 : 0,
-          child: Stack(
-            fit: StackFit.passthrough,
-            children: [
-              if (!kIsWeb) _MobileOverlay(tag: tag),
-              if (kIsWeb) _WebOverlay(tag: tag),
-              if (podCtrs.overlayBuilder != null)
-                GetBuilder<PodGetXVideoController>(
-                  id: 'update-all',
-                  tag: tag,
-                  builder: (podCtr) {
-                    ///Custom overlay
-                    final progressBar = PodProgressBar(
-                      tag: tag,
-                      podProgressBarConfig: podCtr.podProgressBarConfig,
-                    );
-                    final overlayOptions = OverLayOptions(
-                      podVideoState: podCtr.podVideoState,
-                      videoDuration: podCtr.videoDuration,
-                      videoPosition: podCtr.videoPosition,
-                      isFullScreen: podCtr.isFullScreen,
-                      isLooping: podCtr.isLooping,
-                      isOverlayVisible: podCtr.isOverlayVisible,
-                      isMute: podCtr.isMute,
-                      autoPlay: podCtr.autoPlay,
-                      currentVideoPlaybackSpeed: podCtr.currentPaybackSpeed,
-                      videoPlayBackSpeeds: podCtr.videoPlaybackSpeeds,
-                      videoPlayerType: podCtr.videoPlayerType,
-                      podProgresssBar: progressBar,
-                    );
+    return Stack(
+      children: [
+        GetBuilder<PodGetXVideoController>(
+          tag: tag,
+          id: 'overlay',
+          builder: (podCtr) {
+            return AnimatedOpacity(
+              duration: const Duration(milliseconds: 200),
+              opacity: podCtr.isOverlayVisible ? 1 : 0,
+              child: Stack(
+                fit: StackFit.passthrough,
+                children: [
+                  if (!kIsWeb) _MobileOverlay(tag: tag),
+                  if (kIsWeb) _WebOverlay(tag: tag),
+                ],
+              ),
+            );
+          },
+        ),
+        if (podCtrs.overlayBuilder != null)
+          GetBuilder<PodGetXVideoController>(
+            id: 'update-all',
+            tag: tag,
+            builder: (podCtr) {
+              ///Custom overlay
+              final progressBar = PodProgressBar(
+                tag: tag,
+                podProgressBarConfig: podCtr.podProgressBarConfig,
+              );
+              final overlayOptions = OverLayOptions(
+                podVideoState: podCtr.podVideoState,
+                videoDuration: podCtr.videoDuration,
+                videoPosition: podCtr.videoPosition,
+                isFullScreen: podCtr.isFullScreen,
+                isLooping: podCtr.isLooping,
+                isOverlayVisible: podCtr.isOverlayVisible,
+                isMute: podCtr.isMute,
+                autoPlay: podCtr.autoPlay,
+                currentVideoPlaybackSpeed: podCtr.currentPaybackSpeed,
+                videoPlayBackSpeeds: podCtr.videoPlaybackSpeeds,
+                videoPlayerType: podCtr.videoPlayerType,
+                podProgresssBar: progressBar,
+              );
 
-                    return podCtr.overlayBuilder!(overlayOptions);
-                  },
-                )
-            ],
-          ),
-        );
-      },
+              return podCtr.overlayBuilder!(overlayOptions);
+            },
+          )
+      ],
     );
   }
 }
